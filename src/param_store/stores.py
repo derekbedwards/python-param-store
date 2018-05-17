b39f76c22f8d99c4f42bf10ed746d290adf52fdd
Derek Edwards <derekbedwards@gmail.com>
2018-05-17 11:10:08 -0400

    Fixes for ECS

--- src/param_store/stores.py
+++ src/param_store/stores.py
@@ -13,11 +13,19 @@ class EC2ParameterStore(BaseStore):
 
     def __init__(self):
         import boto3
-        import json
-        import urllib2
+        import os
 
-        document = json.loads(urllib2.urlopen("http://169.254.169.254/latest/dynamic/instance-identity/document").read())
-        self.client = boto3.client('ssm', region_name=document['region'])
+        # try to retrieve region locally first
+        region = os.environ.get('AWS_REGION')
+        if region is None:
+            # if we don't have region available via env vars then
+            # retrieve it from instance meta data endpoint
+            import json
+            import urllib2
+            document = json.loads(urllib2.urlopen("http://169.254.169.254/latest/dynamic/instance-identity/document").read())
+            region = document['region']
+
+        self.client = boto3.client('ssm', region_name=region)
 
     def load_values(self, items):
         """Load the parameters from the AWS Parameter Store
